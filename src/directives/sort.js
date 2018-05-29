15dce03bb5c54b271551d3ce0a46d91d6516d2db
lorenzofox3 <laurent34azerty@gmail.com>
2018-05-29 13:23:42 +0200

    fix #6, #7

--- src/directives/sort.js
+++ src/directives/sort.js
@@ -1,18 +1,29 @@
 import {proxyListener} from 'smart-table-events';
 import {TOGGLE_SORT} from '../events';
 
+const debounce = (fn, time) => {
+	let timer = null;
+	return (...args) => {
+		if (timer !== null) {
+			clearTimeout(timer);
+		}
+		timer = setTimeout(() => fn(...args), time);
+	};
+};
+
 const sortListeners = proxyListener({[TOGGLE_SORT]: 'onSortToggle'});
 const directions = ['asc', 'desc'];
 
-export default function ({pointer, table, cycle = false}) {
+export default function ({pointer, table, cycle = false, debounceTime = 0}) {
 	const cycleDirections = cycle === true ? ['none'].concat(directions) : [...directions].reverse();
+	const commit = debounce(table.sort, debounceTime);
 	let hit = 0;
 
 	const directive = Object.assign({
 		toggle() {
 			hit++;
 			const direction = cycleDirections[hit % cycleDirections.length];
-			return table.sort({pointer, direction});
+			return commit({pointer, direction});
 		},
 		state() {
 			return table.getTableState().sort;
@@ -20,10 +31,10 @@ export default function ({pointer, table, cycle = false}) {
 	}, sortListeners({emitter: table}));
 
 	directive.onSortToggle(({pointer: p}) => {
-		if (pointer !== p) {
-			hit = 0;
-		}
+		hit = pointer !== p ? 0 : hit;
 	});
 
+	const {pointer: statePointer, direction = 'asc'} = directive.state();
+	hit = statePointer === pointer ? (direction === 'asc' ? 1 : 2) : 0;
 	return directive;
 }
