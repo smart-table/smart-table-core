c1a48d77f943e9b4528788a72f0819802811ff47
Laurent Renard <laurent@Laurents-MacBook-Pro.local>
2018-10-18 11:19:13 -0400

    remove dits folder from git

--- dist/src/directives/summary.js
+++
@@ -1,3 +0,0 @@
-import { proxyListener } from 'smart-table-events';
-const summaryListener = proxyListener({ ["SUMMARY_CHANGED" /* SUMMARY_CHANGED */]: 'onSummaryChange' });
-export const summaryDirective = ({ table }) => summaryListener({ emitter: table });
--- dist/src/directives/working-indicator.js
+++
@@ -1,3 +0,0 @@
-import { proxyListener } from 'smart-table-events';
-const executionListener = proxyListener({ ["EXEC_CHANGED" /* EXEC_CHANGED */]: 'onExecutionChange' });
-export const workingIndicatorDirective = ({ table }) => executionListener({ emitter: table });
